919e432581855d9777ad1b3babad2672cc199e5d
Jacob Reimers <jacob@reimers.dk>
2016-09-04 13:23:19 +0200

    Start and stop message bus and clean up resources.

--- src/LDN.NodeA/Program.cs
+++ src/LDN.NodeA/Program.cs
@@ -1,29 +1,40 @@
 ﻿namespace LDN.NodeA
 {
     using System;
+    using System.Threading;
     using MassTransit;
 
     class Program
     {
         static void Main(string[] args)
         {
-            /* var bus =*/ Bus.Factory.CreateUsingRabbitMq(
-                  rmq =>
-                  {
-                      var host = rmq.Host(new Uri("rabbitmq://127.0.0.1"), s =>
-                      {
-                          s.Password("ldn");
-                          s.Username("ldn");
-                      });
-                      rmq.UseJsonSerializer();
-                      rmq.ReceiveEndpoint(
-                          host,
-                          "a",
-                          h =>
-                          {
-                              h.Consumer<MessageConsumer>();
-                          });
-                  });
+            var bus = Bus.Factory.CreateUsingRabbitMq(
+                 rmq =>
+                 {
+                     var host = rmq.Host(new Uri("rabbitmq://127.0.0.1"), s =>
+                     {
+                         s.Password("ldn");
+                         s.Username("ldn");
+                     });
+                     rmq.UseJsonSerializer();
+                     rmq.ReceiveEndpoint(
+                         host,
+                         "a",
+                         h =>
+                         {
+                             h.Consumer<MessageConsumer>();
+                         });
+                 });
+            bus.Start();
+            var semaphore = new SemaphoreSlim(1);
+            Console.CancelKeyPress += (_, __) =>
+            {
+                semaphore.Release();
+            };
+            semaphore.Wait();
+
+            semaphore.Dispose();
+            bus.Stop();
         }
     }
 }
