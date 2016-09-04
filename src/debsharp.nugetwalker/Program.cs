105851ecaf05de8d5c46b78a872f2b52b68508db
Jacob Reimers <jacob@reimers.dk>
2016-09-04 21:12:36 +0200

    Change dependency output. Handle arg errors.

--- src/debsharp.nugetwalker/Program.cs
+++ src/debsharp.nugetwalker/Program.cs
@@ -1,34 +1,44 @@
 ﻿namespace debsharp.nugetwalker
 {
-	using System;
-	using System.IO;
-	using System.Linq;
-	using System.Xml.Linq;
-	using NuGet;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+    using NuGet;
 
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			var packageList = Path.GetFullPath (args [0]);
-			var repoAddress = args.Length > 1 ? args [1] : "https://www.nuget.org/api/v2/";
-			var repo = PackageRepositoryFactory.Default.CreateRepository (repoAddress);
-			var packages = XDocument.Load (new FileStream (packageList, FileMode.Open)).Root.Elements("package");
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string packageList;
+            try
+            {
+                packageList = Path.GetFullPath(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine(@"
+usage: debsharp.nugetwalker [path to packages.config file] [(options) URL of nuget source]
+");
+                return;
+            }
+            var repoAddress = args.Length > 1 ? args[1] : "https://www.nuget.org/api/v2/";
+            var repo = PackageRepositoryFactory.Default.CreateRepository(repoAddress);
+            var packages = XDocument.Load(new FileStream(packageList, FileMode.Open)).Root.Elements("package");
 
-			foreach (var packageElement in packages) {
-				var package = repo.FindPackage (packageElement.Attribute ("id").Value);
-				var depSet = package.DependencySets.Any ()
-					? package.DependencySets.Select (x => string.Format (
-					             "{0} depends on {1}", 
-					             package.Id, 
-					             string.Join (
-						             ", ",
-						             x.Dependencies.Select (y => y.Id + ": " + y.VersionSpec))))
-					: new[]{ package.Id + " has no dependencies" };
-				foreach (var set in depSet) {
-					Console.WriteLine (set);
-				}
-			}
-		}
-	}
+            foreach (var packageElement in packages)
+            {
+                var package = repo.FindPackage(packageElement.Attribute("id").Value);
+                var depSet = package.DependencySets.Select(x =>
+                {
+                    var deps = string.Join(" ", x.Dependencies.Select(y => $"{y.Id}-{y.VersionSpec}"));
+                    return $"{package.Id}-{package.Version.ToNormalizedString()}: {deps}";
+                });
+                foreach (var set in depSet)
+                {
+                    Console.WriteLine(set);
+                }
+            }
+        }
+    }
 }
